3da7fa14fa6aefbf06ef4fdf0969957eed27b31f
Basma Elhoseny <basmaelhoseny6@gmail.com>
2024-10-04 12:29:47 +0300

    Add files via upload

--- flask-app/app.py
+++ flask-app/app.py
@@ -30,8 +30,13 @@ os.makedirs(CONVERTED_FOLDER, exist_ok=True)
 
 
 speech2TextPipeline = None
+
 translateEnglish2ArabicPipeline = None
+translateArabic2EnglishPipeline = None
+
 sentimentArabicPipeline = None
+sentimentEnglishPipeline = None
+
 
 # Set the device to 'cuda' if a GPU is available, otherwise use 'cpu'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -40,44 +45,62 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 DEBUG= True
 
 
-def init_models(src_lang,tgt_lang):
+label2word = {
+    "NEG": "negative",
+    "NEU": "neutral",
+    "POS": "positive"
+}
+
+def init_models():
     # Speech2Text
-    audio2textpipe = pipeline("automatic-speech-recognition", model="openai/whisper-medium", device=DEVICE)
+    audio2text_pipe = pipeline("automatic-speech-recognition", model="openai/whisper-medium", device=DEVICE)
 
 
-    # Transaltion
-    if(src_lang == "eng_Latn"):
-            
-        checkpoint = 'facebook/nllb-200-distilled-600M'
-        model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
-        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    # Transaltion            
+    checkpoint = 'facebook/nllb-200-distilled-600M'
+    model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
-        translation_pipeline = pipeline('translation',
-                                    model=model,
-                                    tokenizer=tokenizer,
-                                    src_lang=src_lang,
-                                    tgt_lang=tgt_lang,
-                                    max_length=400,
-                                    device=DEVICE)
-    else:
-        print("NOT IMPLEMENTED..............")
+
+
+    # English --> Arabic
+    english2arabic_translation_pipe = pipeline('translation',
+                                model=model,
+                                tokenizer=tokenizer,
+                                src_lang="eng_Latn",
+                                tgt_lang="arb_Arab",
+                                max_length=400,
+                                device=DEVICE)
+        
+    # Arabic --> English
+
+    arabic2english_translation_pipe = pipeline('translation',
+                            model=model,
+                            tokenizer=tokenizer,
+                            src_lang="arb_Arab",
+                            tgt_lang="eng_Latn",
+                            max_length=400,
+                            device=DEVICE)
+    
+
 
 
     # Sentiment
-    sentiment_pipeline = None
-    if(tgt_lang == "arb_Arab"):
-
-        arabic_model = AutoModelForSequenceClassification.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-da-sentiment")
-        arabic_tokenizer = AutoTokenizer.from_pretrained("CAMeL-Lab/bert-base-arabic-camelbert-da-sentiment")
-        arabic_sentiment_pipe = pipeline('sentiment-analysis', model=arabic_model, tokenizer=arabic_tokenizer, device=DEVICE)
-
-        sentiment_pipeline = arabic_sentiment_pipe
-    else:
-        print("NOT IMPLEMENTED..............")
+    # Arabic 
+    arabic_model_name = "CAMeL-Lab/bert-base-arabic-camelbert-da-sentiment"
+    arabic_tokenizer = AutoTokenizer.from_pretrained(arabic_model_name)
+    arabic_model = AutoModelForSequenceClassification.from_pretrained(arabic_model_name)
+    arabic_sentiment_pipe = pipeline('sentiment-analysis', model=arabic_model, tokenizer=arabic_tokenizer, device=DEVICE)
 
 
+    # English
+    english_model_name = "finiteautomata/bertweet-base-sentiment-analysis"
+    english_tokenizer = AutoTokenizer.from_pretrained(english_model_name)
+    english_model = AutoModelForSequenceClassification.from_pretrained(english_model_name)
+    english_sentiment_pipe = pipeline('sentiment-analysis', model=english_model, tokenizer=english_tokenizer, device=DEVICE)
 
-    return audio2textpipe,translation_pipeline,sentiment_pipeline
+
+    return   audio2text_pipe,  english2arabic_translation_pipe, arabic2english_translation_pipe, arabic_sentiment_pipe,  english_sentiment_pipe
 
 
 # Function to load and preprocess the audio file from a specific path
@@ -86,25 +109,42 @@ def preprocess_audio(audio_path):
     return audio_sample_array, sampling_rate
 
 
-# Function to detect language
+
+# Function 1: Speech to Text using speech2TextPipeline
+def speech_to_text(audio_sample_array, sampling_rate):
+    result = speech2TextPipeline({"array": audio_sample_array, "sampling_rate": sampling_rate}, max_new_tokens=256)
+    return result['text']
+
+
+
+# Function 2: to detect language
 def detect_language(text):
     return detect(text)
 
 
-# Function 1: Speech to Text (English) using openai/whisper-medium
-def speech_to_text_english(pipe,audio_sample_array, sampling_rate):
-    result = pipe({"array": audio_sample_array, "sampling_rate": sampling_rate}, max_new_tokens=256)
-    return result['text']
 
-# Function 2: Translate Text (English to Arabic) using facebook/nllb-200-distilled-600M
-def translate_text_to_arabic(pipline,text):
-    translation = pipline(text)
+# Function 3: Translate Text (English to Arabic) using translateEnglish2ArabicPipeline
+def translate_text_to_arabic(text):
+    translation = translateEnglish2ArabicPipeline(text)
     return translation[0]['translation_text']
 
-# Function 3: Sentiment Analysis (Arabic) using CAMeL-Lab/bert-base-arabic-camelbert-da-sentiment
-def sentiment_analysis_arabic(pipeline,text):
-    sentiment = pipeline(text)
+# Function 4: Translate Text (Arabic to English) using translateArabic2EnglishPipeline
+def translate_text_to_english(text):
+    translation = translateArabic2EnglishPipeline(text)
+    return translation[0]['translation_text']
+
+
+
+# Function 5: Sentiment Analysis (Arabic) using sentimentArabicPipeline
+def sentiment_analysis_arabic(text):
+    sentiment = sentimentArabicPipeline(text)
     return sentiment
+
+# Function 6: Sentiment Analysis (English) using pre-trained sentimentEnglishPipeline
+def sentiment_analysis_english(text):
+    sentiment = sentimentEnglishPipeline(text)
+    return sentiment
+
 
 
 @app.route("/")
@@ -119,7 +159,6 @@ def hello():
 
 @app.route('/run', methods=['POST'])
 def Pipline():
-    print(".........................")
     if 'audio' not in request.files:
         return jsonify({'error': 'No audio file part in the request'}), 400
 
@@ -134,7 +173,6 @@ def Pipline():
     original_filepath = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(original_filepath)
 
-
     transcript = None
     translation = None
     sentiment = None
@@ -142,6 +180,7 @@ def Pipline():
 
     # Convert the audio file to .wav format
     try:
+        # Step 1 Load Audio File Sent
         audio = AudioSegment.from_file(original_filepath)  # Load the file
         converted_filename = f"{os.path.splitext(file.filename)[0]}.wav"
         converted_filepath = os.path.join(CONVERTED_FOLDER, converted_filename)
@@ -152,15 +191,15 @@ def Pipline():
         audio_sample_array, sampling_rate = preprocess_audio(converted_filepath)
 
 
-        # Step 3: Convert the speech to text (assume English for the initial check)
-        transcript = speech_to_text_english(speech2TextPipeline,audio_sample_array, sampling_rate)
+        # Step 3: Convert the speech to text
+        transcript = speech_to_text(audio_sample_array, sampling_rate)
         if DEBUG:
-            print("Speech to Text Output:", transcript)
+            print("Speech to Text Output:", transcript, "\n\n")
 
         # Step 4: Detect language of the transcribed text
         detected_language = detect_language(transcript)
         if DEBUG:
-            print("Detected Language:", detected_language)
+            print("Detected Language:", detected_language, "\n\n")
 
 
         # Step 5: Route based on detected language
@@ -169,44 +208,39 @@ def Pipline():
                 print("Processing English pipeline...")
 
             # Step 6: Translate Text (English to Arabic)
-            translation = translate_text_to_arabic(translateEnglish2ArabicPipeline,transcript)
+            translation = translate_text_to_arabic(transcript)
             if DEBUG:
-                print("Translated Text (English to Arabic):", translation)
+                print("Translated Text (English to Arabic):", translation, "\n\n")
 
             # Step 7: Sentiment Analysis (Arabic)
-            sentiment = sentiment_analysis_arabic(sentimentArabicPipeline,translation)[0]['label']  # CHECK
+            sentiment = sentiment_analysis_arabic(translation)
+            sentiment = f"{sentiment[0]['label']} with score {sentiment[0]['score']}"  # CHECK  NEU With score 0.8
             if DEBUG:
                 print("Sentiment Analysis (Arabic):", sentiment)
 
         elif detected_language == 'ar':  # Arabic language detected
             print("Processing Arabic pipeline...")
-
-            # Step 6: Convert Speech to Text (Arabic)
-            transcript = speech_to_text_arabic(audio_sample_array, sampling_rate)
-            if DEBUG:
-                print("Speech to Text (Arabic):", transcript)
-
-            # Step 7: Translate Text (Arabic to English)
+        
+            # Step 6: Translate Text (Arabic to English)
             translation = translate_text_to_english(transcript)
             if DEBUG:
-                print("Translated Text (Arabic to English):", translation)
+                print("Translated Text (Arabic to English):", translation ,"\n\n")
 
-            # Step 8: Sentiment Analysis (English)
+            # Step 7: Sentiment Analysis (English)
             sentiment = sentiment_analysis_english(translation)
+            sentiment = f"{label2word[sentiment[0]['label']]} with score {sentiment[0]['score']}"  #  HECK  NEU With score 0.8
             if DEBUG:
                 print("Sentiment Analysis (English):", sentiment)
+
 
         else:
             print("Unsupported language detected.")
     
-
-
-
-        # return jsonify({
-        #     'message': 'File uploaded and converted successfully',
-        #     'original_filename': file.filename,
-        #     'converted_filename': converted_filename
-        # }), 200
+        # # return jsonify({
+        # #     'message': 'File uploaded and converted successfully',
+        # #     'original_filename': file.filename,
+        # #     'converted_filename': converted_filename
+        # # }), 200
 
     except Exception as e:
         print("Error:",str(e))
@@ -217,23 +251,14 @@ def Pipline():
 
 
 
-# @app.route('/english', methods=['POST'])
-# def EnglishPipline():
-
-#     return jsonify({'data': ""})
-
-
 if __name__ == '__main__':
-    # Load Models
-
-    # English --> Arabic
-    speech2TextPipeline,translateEnglish2ArabicPipeline,sentimentArabicPipeline = init_models(src_lang = "eng_Latn",tgt_lang="arb_Arab")
-
-
-    # Arabic --> English [TODO]
-
+    print("Flask App Satrted :D")
 
     print("Running on ", DEVICE)
+
+    # Load Models
+    print("Loading Piplines ....")
+    speech2TextPipeline, translateEnglish2ArabicPipeline, translateArabic2EnglishPipeline, sentimentArabicPipeline, sentimentEnglishPipeline =init_models()
 
     # Run
     app.run(host='0.0.0.0', port=8080, debug=True)
